f30dc3c48ef260bb5fc30e0579764ea49d194de2
vladislav <irresistible1997@gmail.com>
2020-03-03 15:22:07 +0600

    task added

---
+++ src/com/tasks/htmlnormalizer/Main.java
@@ -0,0 +1,16 @@
+package com.tasks.htmlnormalizer;
+
+public class Main {
+    public static void main(String[] args) {
+        String sourceHtmlString = "<body>         </body>\n" +
+                "<body>\t</body>\n" +
+                "<a href=\"/test\"></a>\n" +
+                "<html><body></body><style>.class { background: #ff0000; }</style></html>\n" +
+                "<p>example <span class=\"specialWord\">text</span></p>\n" +
+                "<span class='specialWord'>text</span></p>";
+
+        String normalizedHtmlString = HtmlNormalizer.normalize(sourceHtmlString);
+
+        System.out.println(normalizedHtmlString);
+    }
+}
